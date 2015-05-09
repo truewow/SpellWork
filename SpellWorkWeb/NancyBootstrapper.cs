83ad87fe5c93c30302822c0a9001eb4279275af7
Nayd <dnpd.dd@gmail.com>
2015-05-09 16:47:42 +0100

    Enable CORS

---
+++ SpellWorkWeb/NancyBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+
+namespace SpellWorkWeb
+{
+    public class NancyBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            // CORS Enable
+            pipelines.AfterRequest.AddItemToEndOfPipeline(ctx =>
+            {
+                ctx.Response.WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "POST,GET,HEAD")
+                    .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+
+            });
+        }
+    }
+}
