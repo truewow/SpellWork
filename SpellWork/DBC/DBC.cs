0a76e52c7c4bcd7143c94aa5acab1ea0db155d66
funjoker <funjoker109@gmail.com>
2021-04-11 15:42:07 +0200

    Update to 9.0.5 (#42)

--- SpellWork/DBC/DBC.cs
+++ SpellWork/DBC/DBC.cs
@@ -17,7 +17,7 @@ namespace SpellWork.DBC
 {
     public static class DBC
     {
-        public const string Version = "SpellWork 9.0.2 (37176)";
+        public const string Version = "SpellWork 9.0.5 (37899)";
         public const uint MaxLevel = 60;
         public const uint MaxItemLevel = 1300;
 
--- SpellWork/DBC/Structures/RandPropPointsEntry.cs
+++ SpellWork/DBC/Structures/RandPropPointsEntry.cs
@@ -6,8 +6,16 @@ namespace SpellWork.DBC.Structures
     {
         [Index(true)]
         public uint ID;
+        public float DamageReplaceStatF;
+        public float DamageSecondaryF;
         public int DamageReplaceStat;
         public int DamageSecondary;
+        [Cardinality(5)]
+        public float[] EpicF;
+        [Cardinality(5)]
+        public float[] SuperiorF;
+        [Cardinality(5)]
+        public float[] GoodF;
         [Cardinality(5)]
         public uint[] Epic;
         [Cardinality(5)]
